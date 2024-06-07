3ce150626787d10509b1cca9c0ecd13427f91089
TPH <tph@naviworld-asia.com>
2024-06-07 17:21:00 +0700

    test

--- generate_barcode_pdf.py
+++ generate_barcode_pdf.py
@@ -5,7 +5,7 @@ from reportlab.lib.pagesizes import letter
 from PIL import Image
 
 data = "SWHPK2406-01234"
-data2 = "AASD056-05664"
+data2 = "SWHPK2406-01222"
 gs1_data = f"{data}"
 
 def generate_barcode(code, filename):
