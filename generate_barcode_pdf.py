bb02a79219899b7510876142ae057fcfa0455fb9
TPH <tph@naviworld-asia.com>
2024-06-07 13:26:41 +0700

    add barcode.py

---
+++ generate_barcode_pdf.py
@@ -0,0 +1,28 @@
+from barcode import Gs1_128
+from barcode.writer import ImageWriter
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from PIL import Image
+
+data = "PWHPK2406-01256"
+
+gs1_data = f"{data}"
+
+def generate_barcode(code, filename):
+    barcode = Gs1_128(code, writer=ImageWriter())
+    barcode.save(filename)
+    return filename + ".png"
+
+def create_pdf_with_barcode(barcode_image, pdf_filename):
+    c = canvas.Canvas(pdf_filename, pagesize=letter)
+    width, height = letter
+    img = Image.open(barcode_image)
+    img_width, img_height = img.size
+    
+    c.drawImage(barcode_image, (width - img_width) / 2, (height - img_height) / 2)
+    c.showPage()
+    c.save()
+
+# ตัวอย่างการใช้งาน
+barcode_image = generate_barcode(gs1_data, "barcode") 
+create_pdf_with_barcode(barcode_image, "barcode.pdf") 
